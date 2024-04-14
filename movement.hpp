3ffd2fc92bd503d786a91ea89c771ed63d890192
YUMIAO HU <2663221H@STUDENT.GLA.AC.UK>
2024-04-14 21:18:05 +0100

    Add files via upload

---
+++ movement.hpp
@@ -0,0 +1,16 @@
+#ifndef MOVEMENT_H
+#define MOVEMENT_H
+#include <stdio.h>
+#include <iostream>
+#include <pigpio.h>
+#include <unistd.h>
+#include <math.h>
+volatile int countL=0, countR=0 ;
+static int vL_current=0,vR_current=0,target_count=0;
+//a = 1, b = 0; 
+
+static void timercallback(void)
+void callbackL(int wayL);
+void callbackR(int wayR);
+
+#endif
